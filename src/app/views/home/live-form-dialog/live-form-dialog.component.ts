64a502d24d2d261d7c9c6c5b2024aa7676cee45e
Julio Cesar <j.juliocesargoncalves@gmail.com>
2021-04-19 12:07:54 -0300

    Modal para adiconar live

---
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { LiveService } from 'src/app/shared/service/live.service';
+
+@Component({
+  selector: 'app-live-form-dialog',
+  templateUrl: './live-form-dialog.component.html',
+  styleUrls: ['./live-form-dialog.component.css']
+})
+export class LiveFormDialogComponent implements OnInit {
+
+  public liveForm : FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private rest : LiveService,
+    public dialogRef: MatDialogRef<LiveFormDialogComponent>
+  ) { }
+
+  ngOnInit(): void {
+    this.liveForm = this.fb.group({
+      liveName : ['', [Validators.required]],
+      channelName : ['', [Validators.required]],
+      liveDate : ['2021-08-01T20:00:00', [Validators.required]],
+      liveTime : ['', [Validators.required]],
+      liveLink : ['', [Validators.required]],
+
+    });
+  }
+
+  createLive(){
+    this.rest.postLives(this.liveForm.value).subscribe(result => {});
+    this.dialogRef.close();
+    this.liveForm.reset();
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+}
